1328b9d3b25eaab6e29708a2fc7f37c8ea1d09aa
Javier Colmenar <fjcolmenar@serbatic.es>
2022-08-12 13:14:01 +0200

    Corrections.

--- src/main/java/com/openwebinars/_Controladores02/Application.java
+++ src/main/java/com/openwebinars/_Controladores02/Application.java
@@ -1,7 +1,10 @@
 package com.openwebinars._Controladores02;
 
+import com.openwebinars._Controladores02.upload.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -9,6 +12,14 @@ public class Application {
 	public static void main(String[] args) {
 
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
 	}
 
 }
