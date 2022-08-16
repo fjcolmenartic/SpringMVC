91af24facf16c7a966279d657900d7a5498a9391
Javier Colmenar <fjcolmenar@serbatic.es>
2022-08-16 12:09:45 +0200

    Adding security request

--- src/main/java/com/openwebinars/_Controladores02/seguridad/SecurityConfig.java
+++ src/main/java/com/openwebinars/_Controladores02/seguridad/SecurityConfig.java
@@ -23,6 +23,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
+        http
+                .authorizeRequests()
+                    .antMatchers("/webjars/**", "/css/**").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                .formLogin()
+                    .loginPage("/login")
+                    .permitAll();
     }
 }
